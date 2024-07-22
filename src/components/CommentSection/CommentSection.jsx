cd026263b48b20af3b4257e0cfefda81aa3b6890
idaoskooei <idaoskooei@gmail.com>
2024-07-21 23:20:55 -0400

    make default video dynamic

--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -1,41 +1,41 @@
 import './CommentSection.scss';
 import CommentCard from '../CommentCard/CommentCard';
 
-
-const CommentSection = ({selectedVideo}) => {
-
-    let currentVideoComments = selectedVideo?.comments;
+const CommentSection = ({ selectedVideo }) => {
+    const currentVideoComments = selectedVideo?.comments || [];
 
     return (
         <>
-            <h3 className='comment-form__title'>3 Comments</h3>
+            <h3 className='comment-form__title'>{currentVideoComments.length} Comments</h3>
             <section className='comment-form'>
                 <div className='comment-form__avatar'></div>
                 <form className='comment-form__form'>
                     <label htmlFor='newComment' className='comment-form__label'>JOIN THE CONVERSATION</label>
                     <div className='comment-form__nonmobile-container'>
                         <textarea id='newComment' className='comment-form__input' placeholder='Add a new comment'></textarea>
-                        <button 
-                        className='comment-form__button'
-                        onClick={(event)=>event.preventDefault()}
-                        >COMMENT</button>
+                        <button
+                            className='comment-form__button'
+                            onClick={(event) => event.preventDefault()}
+                        >
+                            COMMENT
+                        </button>
                     </div>
                 </form>
             </section>
 
             <section className='comment-section'>
-                {currentVideoComments?.map((comment) => 
+                {currentVideoComments.map((comment) => 
                     <CommentCard        
-                        key = {comment.id} 
-                        commentComment = {comment.comment}
-                        commentName = {comment.name}
-                        commentTimestamp = {comment.timestamp}
+                        key={comment.id} 
+                        commentComment={comment.comment}
+                        commentName={comment.name}
+                        commentTimestamp={comment.timestamp}
                     />
                 )}
                 
             </section>
         </>
     );
-}
+};
 
-export default CommentSection
+export default CommentSection;
--- src/pages/VideoPlayer/VideoPlayer.jsx
+++ src/pages/VideoPlayer/VideoPlayer.jsx
@@ -11,52 +11,57 @@ const baseURL = "https://unit-3-project-api-0a5620414506.herokuapp.com";
 const apiKey = "eb0a0738-fbb2-4a75-a000-e9a8cd7e2827";
 
 const VideoPlayer = () => {
-    const {videoId} = useParams()
-
-    const [videoArray, setVideoArray] = useState();
-    const [selectedVideo, setSelectedVideo] = useState();
+    const { videoId } = useParams();
+    const [videoArray, setVideoArray] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
 
     useEffect(() => {
         const fetchVideos = async () => {
             try {
-                const response =  await axios.get(`${baseURL}/videos?api_key=${apiKey}`);
+                const response = await axios.get(`${baseURL}/videos?api_key=${apiKey}`);
                 setVideoArray(response.data);
-                
+
+                if (!videoId && response.data.length > 0) {
+                    const defaultVideoId = response.data[3].id;
+                    fetchVideoById(defaultVideoId);
+                }
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
         fetchVideos();
-    }, [])
- 
-    useEffect(() => {
-        if(videoId) {
-            axios.get(`${baseURL}/videos/${videoId}?api_key=${apiKey}`)
-            .then(response => setSelectedVideo(response.data))
-            .catch(error=> console.log(error))
-        } else {
-            axios.get(`${baseURL}/videos/b6f35f03-7936-409b-bd2a-446bcc5f30e7?api_key=${apiKey}`)
-            .then(response => setSelectedVideo(response.data))
-            .catch(error=> console.log(error))
+    }, [videoId]);
+
+    const fetchVideoById = async (id) => {
+        try {
+            const response = await axios.get(`${baseURL}/videos/${id}?api_key=${apiKey}`);
+            setSelectedVideo(response.data);
+        } catch (error) {
+            console.log(error);
         }
-        
-    },[videoId])
+    };
+
+    useEffect(() => {
+        if (videoId) {
+            fetchVideoById(videoId);
+        }
+    }, [videoId]);
 
     return (
         <>
-            <Hero selectedVideo = {selectedVideo}/>
+            <Hero selectedVideo={selectedVideo} />
             <section className='container'>
                 <section className='container__left'>
-                <HeroContent  selectedVideo = {selectedVideo}/>
-                <CommentSection selectedVideo = {selectedVideo} />
+                    <HeroContent selectedVideo={selectedVideo} />
+                    <CommentSection selectedVideo={selectedVideo} />
                 </section>
                 <VideoList
-                    videoArray = {videoArray}
-                    selectedVideo = {selectedVideo}
+                    videoArray={videoArray}
+                    selectedVideo={selectedVideo}
                 />
             </section>
         </>
-      )
-}
+    );
+};
 
 export default VideoPlayer;
